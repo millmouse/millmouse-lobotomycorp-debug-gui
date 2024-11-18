429ece3e4deef668dfc26c6a5b6c933d0a629148
millmouse <74473743+millmouse@users.noreply.github.com>
2024-11-17 23:56:21 -0700

    more manager patches

--- inority_TodayOrdeal_MOD/Harmony_Patch.cs
+++ inority_TodayOrdeal_MOD/Harmony_Patch.cs
@@ -2,9 +2,11 @@
 using UnityEngine;
 using Harmony;
 using System.Collections.Generic;
-using SageMod.Patches;
 using System.Collections;
 using GlobalBullet;
+using UnityEngine.UI;
+using WorkerSpine;
+using WorkerSprite;
 
 namespace MyMod
 {
@@ -20,7 +22,8 @@ namespace MyMod
                 new StartGamePatch(mod);
                 GameObject updatePatchObject = new GameObject("UpdatePatchObject");
 
-                CallPatches(mod);
+                PatchCaller patchCaller = new PatchCaller();
+                patchCaller.CallPatches(mod);
 
                 GameObject guiObject = new GameObject("HelloWorldGUI");
                 guiInstance = guiObject.AddComponent<HelloWorldGUI>();
@@ -30,76 +33,6 @@ namespace MyMod
             });
         }
 
-        private void CallPatches(HarmonyInstance mod)
-        {
-            new AnyPatch(mod, typeof(BgmManager), "PlayBgm");
-            new AnyPatch(mod, typeof(OrdealManager), "OnGameInit");
-            new AnyPatch(mod, typeof(GlobalAudioManager), "OnSceneLoad");
-            new AnyPatch(mod, typeof(OrdealManager), "OnStageStart");
-            //new AnyPatch(mod, typeof(CursorManager), "Start");
-            //new AnyPatch(mod, typeof(CursorManager), "HideCursor");
-            //new AnyPatch(mod, typeof(CursorManager), "ForcelyCurserSet");
-            //new AnyPatch(mod, typeof(CursorManager), "OnEnteredTarget");
-            //new AnyPatch(mod, typeof(CursorManager), "OnExitTarget");
-            //new AnyPatch(mod, typeof(CursorManager), "CursorSet");
-            //new AnyPatch(mod, typeof(RandomEventManager), "OnStageStart");
-            //new AnyPatch(mod, typeof(AgentManager), "OnStageStart");
-            //new AnyPatch(mod, typeof(AgentManager), "GetAgentList");
-            //new AnyPatch(mod, typeof(AgentManager), "GetAgentList");
-            //new AnyPatch(mod, typeof(AnimatorManager), "Init");
-            //new AnyPatch(mod, typeof(MissionManager), "Init");
-            //new AnyPatch(mod, typeof(ObserveInfoManager), "get_instance");
-            //new AnyPatch(mod, typeof(OfficerManager), "Init");
-            //new AnyPatch(mod, typeof(HierarchicalDataManager), "Init");
-
-            //new AnyPatch(mod, typeof(GlobalGameManager), "InitHidden");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "InitStoryMode");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "LoadSaveFile");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "ChangeLanguage");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "Start");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "ExistSaveData");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "ExistSaveData");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "IsPlaying");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "OnDisable");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "OnEnable");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "OnLevelWasLoaded");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "LoadGlobalData");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "LoadData");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "SaveData");
-            //new AnyPatch(mod, typeof(GlobalGameManager), "SaveDataWithCheckPoint");
-
-            //new AnyPatch(mod, typeof(GlobalBulletManager), "UpdateUI");
-            //new AnyPatch(mod, typeof(GlobalBulletManager), "ActivateBullet");
-            //new AnyPatch(mod, typeof(GlobalBulletManager), "ExcuteBullet");
-            //new AnyPatch(mod, typeof(GlobalBulletManager), "OnStageStart");
-            //new AnyPatch(mod, typeof(GlobalBulletManager), "SetMaxBullet");
-
-            //new AnyPatch(mod, typeof(ExplodeGutManager), "MakeEffects");
-            //new AnyPatch(mod, typeof(ExplodeGutManager), "Start");
-
-            //new AnyPatch(mod, typeof(CreatureManager), "Init");
-            //new AnyPatch(mod, typeof(CreatureManager), "RegisterCreature");
-            //new AnyPatch(mod, typeof(CreatureManager), "OnGameInit");
-            //new AnyPatch(mod, typeof(CreatureManager), "AddCreature");
-            //new AnyPatch(mod, typeof(CreatureManager), "FindCreature");
-            //new AnyPatch(mod, typeof(CreatureManager), "GetCreature");
-            //new AnyPatch(mod, typeof(CreatureManager), "GetCreatureList");
-            //new AnyPatch(mod, typeof(CreatureManager), "GetObserveInfo");
-            //new AnyPatch(mod, typeof(CreatureManager), "GetSaveData");
-            //new AnyPatch(mod, typeof(CreatureManager), "LoadData");
-            //new AnyPatch(mod, typeof(CreatureManager), "ResetProbReductionCounterAll");
-            //new AnyPatch(mod, typeof(CreatureManager), "TryGetValue");
-            //new AnyPatch(mod, typeof(CreatureManager), "LoadSpecialSkillTable");
-            //new AnyPatch(mod, typeof(CreatureManager), "OnStageStart");
-
-
-            new AgentModelPatch(mod, "OnClick");
-            new AgentModelPatch(mod, "OnEnterRoom");
-            new AgentModelPatch(mod, "OnStageStart");
-
-            //TODO: Onclick agent model print name, rwbp, current sefira, promotion points, which monsters worked on and how many times.
-
-        }
 
         private IEnumerator WaitForDebugTab(HelloWorldGUI guiInstance)
         {
--- inority_TodayOrdeal_MOD/Patches/AnyPatch.cs
+++ inority_TodayOrdeal_MOD/Patches/AnyPatch.cs
@@ -4,7 +4,7 @@ using System;
 using System.Diagnostics;
 using System.Reflection;
 
-namespace SageMod.Patches
+namespace MyMod
 {
     public class AnyPatch
     {
