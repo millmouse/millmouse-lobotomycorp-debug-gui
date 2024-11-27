a21b5e1a17c7688cc1834d5f29380d15396e83cc
millmouse <74473743+millmouse@users.noreply.github.com>
2024-11-26 18:57:07 -0700

    create new AgentOnClickPatch for clicking an agent patch

---
+++ millmouse_DebugConsole_MOD/Patches/AgentOnClickPatch.cs
@@ -0,0 +1,48 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+
+namespace MyMod.Patches
+{
+    public class AgentOnClickPatch
+    {
+        // Class-level readonly field for the target type.
+        private static readonly Type targetType = typeof(AgentModel);
+
+        // Class-level constants for method names.
+        private const string targetMethodName = "OnClick";
+        private const string patchMethodName = "Postfix_LoggerPatch";  
+
+        public AgentOnClickPatch(HarmonyInstance mod)
+        {
+            Patch(mod);
+        }
+
+        private void Patch(HarmonyInstance mod)
+        {
+            // Use the constant field for the target method name.
+            var originalMethod = targetType.GetMethod(targetMethodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Use the constant field for the patch method name.
+            var myPatchMethod = typeof(AgentOnClickPatch).GetMethod(patchMethodName, BindingFlags.Static | BindingFlags.Public);
+
+            if (originalMethod != null)
+            {
+                mod.Patch(originalMethod, null, new HarmonyMethod(myPatchMethod), null);
+            }
+            else
+            {
+                Log.Error($"Failed to find method: {targetMethodName} in class {targetType.Name}");
+            }
+        }
+
+        public static void Postfix_LoggerPatch(AgentModel __instance)
+        {
+            if (Harmony_Patch.guiInstance != null && Harmony_Patch.guiInstance.debugTab != null)
+            {
+                Log.LogAndDebug(
+                "Printed to show that AgentOnClickPatch worked!");
+            }
+        }
+    }
+}
--- millmouse_DebugConsole_MOD/Patches/PatchCaller.cs
+++ millmouse_DebugConsole_MOD/Patches/PatchCaller.cs
@@ -13,50 +13,22 @@ namespace MyMod.Patches
     {
         public void CallPatches(HarmonyInstance mod)
         {
-            PatchAgentManager(mod);
-            PatchResultScreen(mod);
-            PatchAgentModel(mod);
-            PatchAgentHistory(mod);
+            //PatchAgentModel(mod);
+            PatchAgentClick(mod);
+
         }
 
-        private void PatchAgentHistory(HarmonyInstance mod)
+        private void PatchAgentClick(HarmonyInstance mod)
         {
-            new AnyPatch(mod, typeof(AgentHistory), "Disposition");
-            new AnyPatch(mod, typeof(AgentHistory), "AddPanic");
-            new AnyPatch(mod, typeof(AgentHistory), "AddWorkCubeCount");
-            new AnyPatch(mod, typeof(AgentHistory), "AddWorkDay");
-            new AnyPatch(mod, typeof(AgentHistory), "CreatureAttack");
-            new AnyPatch(mod, typeof(AgentHistory), "LoadData");
-            new AnyPatch(mod, typeof(AgentHistory), "GetSaveData");
-            new AnyPatch(mod, typeof(AgentHistory), "Suppress");
-            new AnyPatch(mod, typeof(AgentHistory), "AddWorkSuccess");
-            new AnyPatch(mod, typeof(AgentHistory), "PhysicalDamage");
-            new AnyPatch(mod, typeof(AgentHistory), "MentalDamage");
-
-            new AnyPatch(mod, typeof(PromotionNeedVal), "GetNeedVal");
+            new AgentOnClickPatch(mod);
         }
 
-        private void PatchAgentManager(HarmonyInstance mod)
-        {
-            new AnyPatch(mod, typeof(AgentManager), "OnStageStart");
-        }
-
-        private void PatchResultScreen(HarmonyInstance mod)
-        {
-            new AnyPatch(mod, typeof(ResultScreen), "SoundMake");
-            new AnyPatch(mod, typeof(ResultScreen), "OnEnable");
-            new AnyPatch(mod, typeof(ResultScreen), "OnSuccessManagement");
-            new AnyPatch(mod, typeof(ResultScreen), "CreatureAnimCall");
-            new AnyPatch(mod, typeof(ResultScreen), "OnClickContinueGame");
-            new AnyPatch(mod, typeof(ResultScreen), "AgentReset");
-        }
-
-        private void PatchAgentModel(HarmonyInstance mod)
-        {
-            new AgentModelPatch(mod, "OnClick");
-            new AgentModelPatch(mod, "OnEnterRoom");
-            new AgentModelPatch(mod, "OnStageStart");
-        }
+        //private void PatchAgentModel(HarmonyInstance mod)
+        //{
+        //    new AgentModelPatch(mod, "OnClick");
+        //    new AgentModelPatch(mod, "OnEnterRoom");
+        //    new AgentModelPatch(mod, "OnStageStart");
+        //}
 
     }
 }
