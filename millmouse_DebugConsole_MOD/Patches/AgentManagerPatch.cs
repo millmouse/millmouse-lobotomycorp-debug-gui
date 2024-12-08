8cd71eb715dddf5fe19d066e9dd45b37ecab89f4
millmouse <74473743+millmouse@users.noreply.github.com>
2024-12-08 16:54:15 -0700

    print all agent names on GetAgentList()

--- millmouse_DebugConsole_MOD/Patches/AgentManagerPatch.cs
+++ millmouse_DebugConsole_MOD/Patches/AgentManagerPatch.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 
-namespace SageMod.Patches
+using Harmony;
+using MyMod;
+
+namespace MyMod.Patches
 {
-    internal class AgentManagerPatch
+    public class AgentManagerPatch
     {
+        private static readonly Type targetType = typeof(AgentManager);
+        private const string TargetMethodName = "GetAgentList"; 
+
+        public AgentManagerPatch(HarmonyInstance mod)
+        {
+            string patchMethodName = "Postfix_LoggerPatch";
+            Patch(mod, patchMethodName);
+        }
+
+        private void Patch(HarmonyInstance mod, string patchMethodName)
+        {
+            var originalMethod = targetType.GetMethod(TargetMethodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var myPatchMethod = typeof(AgentManagerPatch).GetMethod(patchMethodName, BindingFlags.Static | BindingFlags.Public);
+
+            if (originalMethod != null)
+            {
+                mod.Patch(originalMethod, null, new HarmonyMethod(myPatchMethod), null);
+            }
+            else
+            {
+                Log.Error($"Failed to find method: {TargetMethodName} in class {targetType.Name}");
+            }
+        }
+
+        public static void Postfix_LoggerPatch(IList<AgentModel> __result)
+        {
+            if (Harmony_Patch.guiInstance?.debugTab != null)
+            {
+                Log.LogAndDebug($"AgentManagerPatch: GetAgentList returned {__result?.Count ?? 0} agents:");
+
+                if (__result != null)
+                {
+                    foreach (var agent in __result)
+                    {
+                        string agentName = agent?.name ?? "Unknown Agent";
+                        Log.LogAndDebug($"- Agent Name: {agentName}");
+                        //Log.LogAndDebug($"- Agent Name: {agentName}");
+                    }
+                }
+            }
+        }
     }
 }
--- millmouse_DebugConsole_MOD/Patches/PatchCaller.cs
+++ millmouse_DebugConsole_MOD/Patches/PatchCaller.cs
@@ -14,7 +14,14 @@ namespace MyMod.Patches
         public void CallPatches(HarmonyInstance mod)
         {
             PatchAgentModel(mod);
+            PatchAgentManager(mod);
         }
+
+        private void PatchAgentManager(HarmonyInstance mod)
+        {
+            new AgentManagerPatch(mod);
+        }
+
         private void PatchAgentModel(HarmonyInstance mod)
         {
             new AgentModelPatch(mod, "OnClick");
